856ed5dcf738920727e90bef0797c6823cea491c
Ravi <ravi@vytec.com>
2023-03-05 16:26:50 +0530

    acr uodaed

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "acr" {
   name                = "containerRegistrydev"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = "Central India"
   sku                 = "Premium"
   admin_enabled       = false
   georeplications {
