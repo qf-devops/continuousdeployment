2b505b8d28b846879322fb1d9dfb5cdf1185a2da
Ravi <ravi@vytec.com>
2023-03-05 19:11:48 +0530

    keyvault

---
+++ terraform/keyvault.tf
@@ -0,0 +1,32 @@
+resource "azurerm_key_vault" "kv" {
+  name                       = "examplekeyvault"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "secret-sauce"
+  value        = "szechuan"
+  key_vault_id = azurerm_key_vault.kv.id
+}
