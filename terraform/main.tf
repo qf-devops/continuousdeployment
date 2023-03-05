9ef2c417eccf27cd5a1adc093e010d6a890045f2
Ravi <ravi@vytec.com>
2023-03-05 12:29:50 +0530

    uopdated oid

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id   = "31a90186-a097-4b71-9527-2ce644c05cb4"
+  tenant_id         = "e80326a2-653b-4be1-9c42-fa8aa110e0f6"
+  client_id         = "0c0d79da-94d1-4561-b9b6-73aa7498c27b"
+  client_secret     = "6uo8Q~hdkl5dQRcHx20lpe1s6imzY.NMj0xcHcR0"
 }
 
 resource "azurerm_resource_group" "rg" {
